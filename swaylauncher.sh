9908c7e8ef63495aecfffcd423c37dc2eb472275
Michał Radwański <enedil@outlook.com>
2023-11-13 00:31:41 +0100

    actually kill old process

--- swaylauncher.sh
+++ swaylauncher.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR"
 
-pkill telegramchatstats-sampler
+pkill --full "telegramchatstats-sampler sample"
 RUST_LOG=trace target/release/telegramchatstats-sampler sample
